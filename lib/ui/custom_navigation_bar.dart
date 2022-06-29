d6f62fa6ddeaf6e6c581c329da289e4a4f9c621b
Julia Herold <j.herold@student.tugraz.at>
2022-06-29 20:23:26 +0200

    Static analysis

--- lib/ui/custom_navigation_bar.dart
+++ lib/ui/custom_navigation_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:paintroid/ui/color_schemes.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   const CustomNavigationBar({Key? key}) : super(key: key);
