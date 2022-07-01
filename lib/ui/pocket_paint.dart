4535d83e50ac7c57230efbaeca3d0464e8775255
Ikroop Singh Kalsi <ikroop.singh.kalsi@gmail.com>
2022-07-01 21:33:40 +0530

    PAINTROID-449 Add Fullscreen mode (#2)
    * add toggle fullscreen feature
    
    * hide toggle-fullscreen button when drawing in fullscreen mode
    
    * add fullscreen tests
    
    * add widget tests to github workflow, join up jobs
    
    * archive release apk artifact in github CI

--- lib/ui/pocket_paint.dart
+++ lib/ui/pocket_paint.dart
@@ -1,18 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'drawing_board.dart';
 
-class PocketPaint extends StatelessWidget {
+class PocketPaint extends StatefulWidget {
   final String title;
 
   const PocketPaint({Key? key, required this.title}) : super(key: key);
 
   @override
+  State<PocketPaint> createState() => _PocketPaintState();
+}
+
+class _PocketPaintState extends State<PocketPaint> {
+  bool _isFullscreen = false;
+  bool _isDrawing = false;
+
+  void _toggleOnFullscreen() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    setState(() => _isFullscreen = true);
+  }
+
+  void _toggleOffFullscreen() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    setState(() => _isFullscreen = false);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title), centerTitle: false),
-      body: const DrawingBoard(),
-      bottomNavigationBar: const _BottomNavigationBar(),
+    return WillPopScope(
+      onWillPop: () async {
+        final willPop = !_isFullscreen;
+        if (_isFullscreen) {
+          _toggleOffFullscreen();
+        }
+        return willPop;
+      },
+      child: Scaffold(
+        appBar: _isFullscreen
+            ? null
+            : AppBar(
+                title: Text(widget.title),
+                centerTitle: false,
+                actions: [
+                  IconButton(
+                    onPressed: () => _toggleOnFullscreen(),
+                    icon: const Icon(Icons.fullscreen),
+                  )
+                ],
+              ),
+        body: SafeArea(
+          child: Stack(
+            children: [
+              DrawingBoard(
+                startedDrawing: () => setState(() => _isDrawing = true),
+                stoppedDrawing: () => setState(() => _isDrawing = false),
+              ),
+              if (_isFullscreen)
+                Positioned(
+                  top: 2,
+                  right: 2,
+                  child: AnimatedOpacity(
+                    opacity: _isDrawing ? 0 : 1,
+                    duration: const Duration(milliseconds: 200),
+                    child: IconButton(
+                      onPressed: () => _toggleOffFullscreen(),
+                      icon: const Icon(Icons.fullscreen_exit),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+        bottomNavigationBar:
+            _isFullscreen ? null : const _BottomNavigationBar(),
+      ),
     );
   }
 }
---
+++ test/widget/ui/pocket_paint_test.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:paintroid/ui/pocket_paint.dart';
+
+void main() {
+  late String testTitle;
+  late Widget sut;
+
+  setUp(() {
+    testTitle = "Test Title";
+    sut = MaterialApp(
+      home: PocketPaint(title: testTitle),
+    );
+  });
+
+  testWidgets('Should have a top and bottom app bar', (tester) async {
+    await tester.pumpWidget(sut);
+    expect(find.byType(AppBar), findsOneWidget);
+    expect(find.byType(BottomNavigationBar), findsOneWidget);
+  });
+
+  testWidgets('Should have the title in app bar', (tester) async {
+    await tester.pumpWidget(sut);
+    final titleFinder = find.widgetWithText(AppBar, testTitle);
+    expect(titleFinder, findsOneWidget);
+  });
+
+  group('Fullscreen functionality', () {
+    final enterFullscreenButtonFinder =
+        find.widgetWithIcon(IconButton, Icons.fullscreen);
+    final exitFullscreenButtonFinder =
+        find.widgetWithIcon(IconButton, Icons.fullscreen_exit);
+
+    testWidgets(
+      'Should have a fullscreen button in AppBar',
+      (tester) async {
+        await tester.pumpWidget(sut);
+        expect(enterFullscreenButtonFinder, findsOneWidget);
+        final buttonAncestorFinder = find.ancestor(
+          of: enterFullscreenButtonFinder,
+          matching: find.byType(AppBar),
+        );
+        expect(buttonAncestorFinder, findsOneWidget);
+      },
+    );
+
+    group('After going fullscreen', () {
+      testWidgets(
+        'Should hide top and bottom bar',
+        (tester) async {
+          await tester.pumpWidget(sut);
+          final buttonFinder =
+              find.widgetWithIcon(IconButton, Icons.fullscreen);
+          await tester.tap(buttonFinder);
+          await tester.pumpAndSettle();
+          expect(find.byType(AppBar), findsNothing);
+          expect(find.byType(BottomNavigationBar), findsNothing);
+        },
+      );
+
+      testWidgets(
+        'Should have an exit fullscreen button',
+        (tester) async {
+          await tester.pumpWidget(sut);
+          await tester.tap(enterFullscreenButtonFinder);
+          await tester.pumpAndSettle();
+          expect(exitFullscreenButtonFinder, findsOneWidget);
+        },
+      );
+    });
+
+    group('After exiting fullscreen', () {
+      testWidgets(
+        'Should show top and bottom bar',
+        (tester) async {
+          await tester.pumpWidget(sut);
+          await tester.tap(enterFullscreenButtonFinder);
+          await tester.pumpAndSettle();
+          await tester.tap(exitFullscreenButtonFinder);
+          await tester.pumpAndSettle();
+          expect(find.byType(AppBar), findsOneWidget);
+          expect(find.byType(BottomNavigationBar), findsOneWidget);
+        },
+      );
+    });
+  });
+}
