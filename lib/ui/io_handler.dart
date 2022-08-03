eaec9069d10046931a41e6ad17390804815092c8
Ikroop Singh Kalsi <ikroop.singh.kalsi@gmail.com>
2022-08-04 00:12:06 +0530

    refactor io handling

--- lib/ui/io_handler.dart
+++ lib/ui/io_handler.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:oxidized/oxidized.dart';
@@ -12,38 +15,95 @@ class IOHandler {
 
   static final provider = Provider((ref) => IOHandler(ref));
 
-  Future<void> loadImage(ImageLocation location) async {
-    switch (location) {
-      case ImageLocation.photos:
-        await _loadFromPhotos();
-        break;
-      case ImageLocation.files:
-        await _loadFromFiles();
-        break;
+  /// Returns [true] if the image was saved successfully
+  Future<bool> saveImage(BuildContext context) async {
+    final workspaceStateNotifier = ref.read(WorkspaceState.provider.notifier);
+    final imageData = await showSaveImageDialog(context);
+    if (imageData == null) return false;
+    final didSave = await workspaceStateNotifier
+        .performIOTask(() => _saveImageWith(imageData));
+    if (!didSave) return false;
+    workspaceStateNotifier.updateLastSavedCommandCount();
+    return true;
+  }
+
+  /// Returns [true] if -
+  /// - There was no unsaved work, or
+  /// - The unsaved work was saved successfully
+  Future<bool> handleUnsavedChanges(BuildContext context, State state) async {
+    final workspaceStateNotifier = ref.read(WorkspaceState.provider.notifier);
+    if (!workspaceStateNotifier.hasSavedLastWork) {
+      final shouldDiscard = await showDiscardChangesDialog(context);
+      if (shouldDiscard == null || !state.mounted) return false;
+      if (!shouldDiscard) {
+        final didSave = await saveImage(context);
+        if (!didSave) return false;
+      }
+    }
+    return true;
+  }
+
+  /// Returns [true] if the image was loaded successfully
+  Future<bool> loadImage(BuildContext context, State state) async {
+    final shouldContinue = await handleUnsavedChanges(context, state);
+    if (!shouldContinue) return false;
+    if (Platform.isIOS) {
+      if (!state.mounted) return false;
+      final location = await showLoadImageDialog(context);
+      if (location == null) return false;
+      return ref
+          .read(WorkspaceState.provider.notifier)
+          .performIOTask(() => _loadImageFrom(location));
+    } else {
+      return ref
+          .read(WorkspaceState.provider.notifier)
+          .performIOTask(() => _loadImageFrom(ImageLocation.files));
     }
   }
 
-  Future<void> _loadFromPhotos() async {
+  /// Returns [true] if a new image canvas was created successfully
+  Future<bool> newImage(BuildContext context, State state) async {
+    final shouldContinue = await handleUnsavedChanges(context, state);
+    if (!shouldContinue) return false;
+    ref.read(CanvasState.provider.notifier)
+      ..clearBackgroundImageAndResetDimensions()
+      ..resetCanvasWithNewCommands([]);
+    ref.read(WorkspaceState.provider.notifier).updateLastSavedCommandCount();
+    return true;
+  }
+
+  Future<bool> _loadImageFrom(ImageLocation location) async {
+    switch (location) {
+      case ImageLocation.photos:
+        return _loadFromPhotos();
+      case ImageLocation.files:
+        return _loadFromFiles();
+    }
+  }
+
+  Future<bool> _loadFromPhotos() async {
     final loadImage = ref.read(LoadImageFromPhotoLibrary.provider);
     final result = await loadImage();
-    result.when(
+    return result.when(
       ok: (img) async {
         ref.read(CanvasState.provider.notifier)
           ..setBackgroundImage(img)
           ..resetCanvasWithNewCommands([]);
+        return true;
       },
-      err: (failure) {
+      err: (failure) async {
         if (failure != LoadImageFailure.userCancelled) {
           showToast(failure.message);
         }
+        return false;
       },
     );
   }
 
-  Future<void> _loadFromFiles() async {
+  Future<bool> _loadFromFiles() async {
     final loadImage = ref.read(LoadImageFromFileManager.provider);
     final result = await loadImage();
-    result.when(
+    return result.when(
       ok: (imageFromFile) async {
         final canvasStateNotifier = ref.read(CanvasState.provider.notifier);
         if (imageFromFile.catrobatImage != null) {
@@ -56,34 +116,43 @@ class IOHandler {
             ? canvasStateNotifier.clearBackgroundImageAndResetDimensions()
             : canvasStateNotifier
                 .setBackgroundImage(imageFromFile.rasterImage!);
+        return true;
       },
       err: (failure) {
         if (failure != LoadImageFailure.userCancelled) {
           showToast(failure.message);
         }
+        return false;
       },
     );
   }
 
-  Future<void> saveImage(ImageMetaData imageData) async {
+  Future<bool> _saveImageWith(ImageMetaData imageData) async {
     if (imageData is JpgMetaData || imageData is PngMetaData) {
-      await _saveAsRasterImage(imageData);
+      return _saveAsRasterImage(imageData);
     } else if (imageData is CatrobatImageMetaData) {
-      await _saveAsCatrobatImage(imageData);
+      return _saveAsCatrobatImage(imageData);
     }
+    return false;
   }
 
-  Future<void> _saveAsRasterImage(ImageMetaData imageData) async {
+  Future<bool> _saveAsRasterImage(ImageMetaData imageData) async {
     final image = await ref
         .read(RenderImageForExport.provider)
         .call(keepTransparency: imageData.format != ImageFormat.jpg);
-    await ref.read(SaveAsRasterImage.provider).call(imageData, image).when(
-          ok: (_) => showToast("Saved to Photos"),
-          err: (failure) => showToast(failure.message),
-        );
+    return ref.read(SaveAsRasterImage.provider).call(imageData, image).when(
+      ok: (_) {
+        showToast("Saved to Photos");
+        return true;
+      },
+      err: (failure) {
+        showToast(failure.message);
+        return false;
+      },
+    );
   }
 
-  Future<void> _saveAsCatrobatImage(CatrobatImageMetaData imageData) async {
+  Future<bool> _saveAsCatrobatImage(CatrobatImageMetaData imageData) async {
     final commands = ref.read(CommandManager.provider).history;
     final canvasState = ref.read(CanvasState.provider);
     final imgWidth = canvasState.size.width.toInt();
@@ -92,9 +161,15 @@ class IOHandler {
         commands, imgWidth, imgHeight, canvasState.backgroundImage);
     final saveAsCatrobatImage = ref.read(SaveAsCatrobatImage.provider);
     final result = await saveAsCatrobatImage(imageData, catrobatImage);
-    result.when(
-      ok: (file) => showToast("Saved successfully"),
-      err: (failure) => showToast(failure.message),
+    return result.when(
+      ok: (file) {
+        showToast("Saved successfully");
+        return true;
+      },
+      err: (failure) {
+        showToast(failure.message);
+        return false;
+      },
     );
   }
 }
--- lib/ui/overflow_menu.dart
+++ lib/ui/overflow_menu.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:paintroid/io/io.dart';
 import 'package:paintroid/ui/io_handler.dart';
 import 'package:paintroid/workspace/workspace.dart';
 
@@ -51,69 +48,17 @@ class _OverflowMenuState extends ConsumerState<OverflowMenu> {
         _enterFullscreen();
         break;
       case OverflowMenuOption.saveImage:
-        _saveImage();
+        ioHandler.saveImage(context);
         break;
       case OverflowMenuOption.loadImage:
-        _loadImage();
+        ioHandler.loadImage(context, this);
         break;
       case OverflowMenuOption.newImage:
-        _newImage();
+        ioHandler.newImage(context, this);
         break;
     }
   }
 
   void _enterFullscreen() =>
       ref.read(WorkspaceState.provider.notifier).toggleFullscreen(true);
-
-  Future<void> _saveImage() async {
-    final workspaceStateNotifier = ref.read(WorkspaceState.provider.notifier);
-    final imageData = await showSaveImageDialog(context);
-    if (imageData == null) return;
-    workspaceStateNotifier.performIOTask(() => ioHandler.saveImage(imageData));
-    workspaceStateNotifier.updateLastSavedCommandCount();
-  }
-
-  /// Returns [true] if user didn't tap outside of any dialogs
-  /// or if there is no unsaved work
-  Future<bool> _handleUnsavedChanges() async {
-    final workspaceStateNotifier = ref.read(WorkspaceState.provider.notifier);
-    if (!workspaceStateNotifier.hasSavedLastWork) {
-      final shouldDiscard = await showDiscardChangesDialog(context);
-      if (shouldDiscard == null || !mounted) return false;
-      if (!shouldDiscard) {
-        final imageData = await showSaveImageDialog(context);
-        if (imageData == null) return false;
-        workspaceStateNotifier
-            .performIOTask(() => ioHandler.saveImage(imageData));
-        workspaceStateNotifier.updateLastSavedCommandCount();
-      }
-    }
-    return true;
-  }
-
-  Future<void> _loadImage() async {
-    final shouldContinue = await _handleUnsavedChanges();
-    if (!shouldContinue) return;
-    if (Platform.isIOS) {
-      if (!mounted) return;
-      final location = await showLoadImageDialog(context);
-      if (location == null) return;
-      ref
-          .read(WorkspaceState.provider.notifier)
-          .performIOTask(() => ioHandler.loadImage(location));
-    } else {
-      ref
-          .read(WorkspaceState.provider.notifier)
-          .performIOTask(() => ioHandler.loadImage(ImageLocation.files));
-    }
-  }
-
-  Future<void> _newImage() async {
-    final shouldContinue = await _handleUnsavedChanges();
-    if (!shouldContinue) return;
-    ref.read(CanvasState.provider.notifier)
-      ..clearBackgroundImageAndResetDimensions()
-      ..resetCanvasWithNewCommands([]);
-    ref.read(WorkspaceState.provider.notifier).updateLastSavedCommandCount();
-  }
 }
