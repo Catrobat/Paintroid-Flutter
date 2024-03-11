f17b9fd98a43c9b160520330d22a199fc9f27b2f
Bhav Khurana <bhav.khurana.cse21@itbhu.ac.in>
2024-03-11 20:03:11 +0530

    debug test error

--- packages/colorpicker/lib/src/colorpicker.dart
+++ packages/colorpicker/lib/src/colorpicker.dart
@@ -28,63 +28,58 @@ class ColorPicker extends ConsumerWidget {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(16.0),
       ),
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ColorComparison(
-              currentColor: currentColor,
-              newColor: colorPickerStateData.currentColor
-                  .withOpacity(colorPickerStateData.currentOpacity),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ColorComparison(
+            currentColor: currentColor,
+            newColor: colorPickerStateData.currentColor
+                .withOpacity(colorPickerStateData.currentOpacity),
+          ),
+          GridView.count(
+            childAspectRatio: 1.4,
+            crossAxisCount: 4,
+            crossAxisSpacing: 2.0,
+            mainAxisSpacing: 2.0,
+            shrinkWrap: true,
+            children: List.generate(
+              colors.length + 1,
+              (index) {
+                if (index == colors.length) {
+                  return const CheckerboardSquare();
+                } else {
+                  return ColorSquare(
+                    color: colors[index],
+                    opacity: colorPickerStateData.currentOpacity,
+                  );
+                }
+              },
             ),
-            const SizedBox(height: 10.0),
-            GridView.count(
-              childAspectRatio: 1.4,
-              crossAxisCount: 4,
-              crossAxisSpacing: 2.0,
-              mainAxisSpacing: 2.0,
-              shrinkWrap: true,
-              children: List.generate(
-                colors.length + 1,
-                (index) {
-                  if (index == colors.length) {
-                    return const CheckerboardSquare();
-                  } else {
-                    return ColorSquare(
-                      color: colors[index],
-                      opacity: colorPickerStateData.currentOpacity,
-                    );
-                  }
+          ),
+          OpacitySlider(
+            gradientColor: colorPickerStateData.currentColor,
+          ),
+          Row(
+            children: [
+              const Spacer(),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
                 },
+                child: const Text('CANCEL'),
               ),
-            ),
-            const SizedBox(height: 20.0),
-            OpacitySlider(
-              gradientColor: colorPickerStateData.currentColor,
-            ),
-            const SizedBox(height: 20.0),
-            Row(
-              children: [
-                const Spacer(),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: const Text('CANCEL'),
-                ),
-                const SizedBox(width: 10.0),
-                TextButton(
-                  onPressed: () {
-                    onColorChanged(colorPickerStateData.currentColor
-                        .withOpacity(colorPickerStateData.currentOpacity));
-                    Navigator.pop(context);
-                  },
-                  child: const Text('APPLY'),
-                ),
-              ],
-            )
-          ],
-        ),
+              const SizedBox(width: 10.0),
+              TextButton(
+                onPressed: () {
+                  onColorChanged(colorPickerStateData.currentColor
+                      .withOpacity(colorPickerStateData.currentOpacity));
+                  Navigator.pop(context);
+                },
+                child: const Text('APPLY'),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
