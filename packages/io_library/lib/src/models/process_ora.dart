49e85c3e34f1103f0536901ccb4b4c3708aabbf1
vishad-tyagi <78733360+vishad-tyagi@users.noreply.github.com>
2024-03-23 00:57:03 +0530

    Delete comments

--- packages/io_library/lib/src/models/process_ora.dart
+++ packages/io_library/lib/src/models/process_ora.dart
@@ -9,19 +9,11 @@ class ProcessOra {
 
     for (var file in archive) {
       if (file.isFile && (file.name.endsWith('.png') || file.name.endsWith('.jpg')) || file.name.endsWith('.ora')) {
-        // Decode the image using the image package
-        print("Processing layer: ${file.name}");
         img.Image? decodedImage = img.decodeImage(file.content as List<int>);
 
         if (decodedImage != null) {
-          // Convert the img.Image to ui.Image
-          print("Decoded layer: ${file.name}");
           ui.Image layer = await convertImgImageToUiImage(decodedImage);
           layers.add(layer);
-          print("Converted layer: ${file.name}");
-        }
-        else {
-          print("Failed to decode layer: ${file.name}");
         }
       }
     }
@@ -30,11 +22,9 @@ class ProcessOra {
   }
 
   Future<ui.Image> convertImgImageToUiImage(img.Image image) async {
-    // This function converts an img.Image object to a ui.Image object.
-    // Encode the image to a PNG
     List<int> pngBytes = img.encodePng(image);
 
-    // Use a codec to decode the PNG bytes to a ui.Image
+
     final codec = await ui.instantiateImageCodec(Uint8List.fromList(pngBytes));
     final frame = await codec.getNextFrame();
     return frame.image;
