c47fef8ab397122c2d14c4bb244081383e67f722
Bhav Khurana <bhav.khurana.cse21@itbhu.ac.in>
2024-05-18 17:46:56 +0530

    sort imports, remove duplicates

--- lib/ui/pages/workspace_page/components/bottom_bar/bottom_nav_bar.dart
+++ lib/ui/pages/workspace_page/components/bottom_bar/bottom_nav_bar.dart
@@ -1,9 +1,9 @@
 // Flutter imports:
-import 'package:colorpicker/colorpicker.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:colorpicker/colorpicker.dart';
 
 // Project imports:
 import 'package:paintroid/core/enums/tool_types.dart';
