096ac430444c13bd48137c92052e13c789cfb232
Bhav Khurana <bhav.khurana.cse21@itbhu.ac.in>
2024-05-18 18:13:10 +0530

    fix lint errors

--- packages/colorpicker/lib/src/state/color_picker_state_data.freezed.dart
+++ packages/colorpicker/lib/src/state/color_picker_state_data.freezed.dart
@@ -118,7 +118,7 @@ class _$_ColorPickerStateData implements _ColorPickerStateData {
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is _$_ColorPickerStateData &&
