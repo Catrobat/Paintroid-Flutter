96cf727b99fb4a560fdb785773c3f51f0f4ef4a8
Lenkomotive <mario-kaurin@outlook.com>
2024-06-01 18:04:35 +0200

    PAINTROID-660 - fix CR comments - file for keys

--- lib/ui/pages/workspace_page/components/drawing_surface/canvas_painter.dart
+++ lib/ui/pages/workspace_page/components/drawing_surface/canvas_painter.dart
@@ -1,9 +1,7 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
-
 // Package imports:
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 // Project imports:
 import 'package:paintroid/core/commands/command_manager/command_manager_provider.dart';
 import 'package:paintroid/core/commands/command_painter.dart';
@@ -11,6 +9,7 @@ import 'package:paintroid/core/providers/object/canvas_dirty_notifier.dart';
 import 'package:paintroid/core/providers/state/canvas_state_provider.dart';
 import 'package:paintroid/core/providers/state/checkmark_clicked_state.dart';
 import 'package:paintroid/core/providers/state/tools/toolbox/toolbox_state_provider.dart';
+import 'package:paintroid/core/utils/widget_identifier.dart';
 import 'package:paintroid/ui/pages/workspace_page/components/drawing_surface/checkerboard_pattern.dart';
 
 class CanvasPainter extends ConsumerWidget {
@@ -20,7 +19,7 @@ class CanvasPainter extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final size = ref.watch(canvasStateProvider.select((state) => state.size));
     return Container(
-      key: const ValueKey('CanvasPainter'),
+      key: const ValueKey(WidgetIdentifier.canvasPainter),
       width: size.width,
       height: size.height,
       foregroundDecoration: const BoxDecoration(
--- test/utils/widget_finder.dart
+++ test/utils/widget_finder.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:paintroid/core/utils/widget_identifier.dart';
 import 'package:paintroid/ui/pages/workspace_page/components/bottom_bar/bottom_nav_bar_items.dart';
 import 'package:paintroid/ui/utils/top_bar_action_data.dart';
 
 class WidgetFinder {
-  static final Finder canvas = find.byKey(const ValueKey('CanvasPainter'));
+  static final Finder canvas =
+      find.byKey(const ValueKey(WidgetIdentifier.canvasPainter));
   static final Finder checkMark =
       find.byKey(ValueKey(TopBarActionData.CHECKMARK.name));
   static final Finder plusButton =
@@ -12,5 +14,5 @@ class WidgetFinder {
   static final Finder toolsTab =
       find.byKey(const ValueKey(BottomNavBarItem.TOOLS));
   static final Finder newImageButton =
-      find.byKey(const ValueKey('NewImageActionButton'));
+      find.byKey(const ValueKey(WidgetIdentifier.newImageActionButton));
 }
