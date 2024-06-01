cb00010dc560063e56eaa7c91dc7290e6f64f874
Lenkomotive <mario-kaurin@outlook.com>
2024-06-01 18:54:19 +0200

    PAINTROID-660 - fix CR comments - distance calc to util

--- lib/core/tools/line_tool/vertex.dart
+++ lib/core/tools/line_tool/vertex.dart
@@ -1,11 +1,9 @@
 // Dart imports:
-import 'dart:math';
-
 // Flutter imports:
 import 'package:flutter/material.dart';
-
 // Project imports:
 import 'package:paintroid/core/commands/command_implementation/graphic/line_command.dart';
+import 'package:paintroid/core/utils/distance_calculator.dart';
 
 class Vertex {
   Vertex(
@@ -30,14 +28,11 @@ class Vertex {
   }
 
   bool wasClicked(Offset clickedCoordinate) {
-    double distanceFromCenter = calculateDistance(vertexCenter.dx,
-        vertexCenter.dy, clickedCoordinate.dx, clickedCoordinate.dy);
+    double distanceFromCenter =
+        DistanceCalculator.calculateDistanceBetweenTwoPoints(vertexCenter.dx,
+            vertexCenter.dy, clickedCoordinate.dx, clickedCoordinate.dy);
     if (distanceFromCenter <= VERTEX_RADIUS) return true;
     return false;
-  }
-
-  double calculateDistance(double x0, double y0, double x1, double y1) {
-    return sqrt(pow(x1 - x0, 2) + pow(y1 - y0, 2));
   }
 
   static Paint getVertexPaint() {
