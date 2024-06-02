f50a0c508aa29d72f064ec73f05f8387ab09e803
Mark Šeško <kamenseksesko@student.tugraz.at>
2024-06-02 19:16:30 +0200

    Implement new color names suggestion

--- lib/ui/theme/data/custom_colors.dart
+++ lib/ui/theme/data/custom_colors.dart
@@ -2,35 +2,43 @@
 import 'package:flutter/material.dart';
 
 abstract class CustomColors {
-  static const primary = Color(0xFF0097A7);
-  static const onPrimary = Color(0xFF00363D);
-  static const primaryContainer = Color(0xFF004F58);
-  static const onPrimaryContainer = Color(0xFF97F0FF);
-  static const secondary = Color(0xFFB1CBD0);
-  static const onSecondary = Color(0xFF1C3438);
-  static const secondaryContainer = Color(0xFF334B4F);
-  static const onSecondaryContainer = Color(0xFFCDE7EC);
-  static const tertiary = Color(0xFF4CD9DF);
-  static const onTertiary = Color(0xFF003739);
-  static const tertiaryContainer = Color(0xFF004F52);
-  static const onTertiaryContainer = Color(0xFF6FF6FC);
-  static const error = Color(0xFFFF5454);
-  static const orange = Color(0xFFFFAB08);
-  static const errorContainer = Color(0xFF93000A);
-  static const onError = Color(0xFF690005);
-  static const onErrorContainer = Color(0xFFFFDAD6);
-  static const background = Color(0xFFFFF6F6);
-  static const onBackground = Color(0xFF191C1D);
-  static const surface = Color(0xFF0097A7);
-  static const onSurface = Color(0xFFFFFFFF);
-  static const surfaceVariant = Color(0xFF3F484A);
-  static const onSurfaceVariant = Color(0xFFBFC8CA);
-  static const outline = Color(0xFF899294);
-  static const onInverseSurface = Color(0xFF191C1D);
-  static const inverseSurface = Color(0xFFE1E3E3);
-  static const inversePrimary = Color(0xFF006874);
-  static const shadow = Color(0xFF000000);
-  static const surfaceTint = Color(0xFF4FD8EB);
+  static const oceanBlue = Color(0xFF0097A7);
+  static const seaBlue = Color(0xFF0097A7);
+
+  static const deepTeal = Color(0xFF00363D);
+  static const mutedTeal = Color(0xFF334B4F);
+  static const oceanTeal = Color(0xFF006874);
+
+  static const darkCyan = Color(0xFF004F58);
+  static const lightCyan = Color(0xFF97F0FF);
+
+  static const deepAqua = Color(0xFF004F52);
+  static const paleAqua = Color(0xFF6FF6FC);
+  static const aquaTint = Color(0xFF4FD8EB);
+
+  static const brightTurquoise = Color(0xFF4CD9DF);
+  static const darkSeaGreen = Color(0xFF003739);
+
+  static const vibrantOrange = Color(0xFFFFAB08);
+  static const vividRed = Color(0xFFFF5454);
+  static const deepMaroon = Color(0xFF93000A);
+  static const darkRed = Color(0xFF690005);
+
+  static const pureWhite = Color(0xFFFFFFFF);
+  static const lightCoral = Color(0xFFFFDAD6);
+  static const lightBlush = Color(0xFFFFF6F6);
+
+  static const lightBlueGray = Color(0xFFCDE7EC);
+  static const lightSlateGray = Color(0xFFBFC8CA);
+  static const softBlueGray = Color(0xFFB1CBD0);
+  static const mediumGray = Color(0xFF899294);
+  static const lightGray = Color(0xFFE1E3E3);
+  static const slateGray = Color(0xFF3F484A);
+  static const darkSlateGray = Color(0xFF1C3438);
+
+  static const darkCharcoal = Color(0xFF191C1D);
+  static const deepCharcoal = Color(0xFF191C1D);
+  static const jetBlack = Color(0xFF000000);
 }
 
 extension ToMaterialColor on Color {
--- lib/ui/theme/data/paintroid_theme_data.dart
+++ lib/ui/theme/data/paintroid_theme_data.dart
@@ -64,19 +64,19 @@ abstract class PaintroidThemeData {
           fontSize: FontSize.xLarge,
           fontFamily: PaintroidThemeData.fontFamily,
           fontWeight: FontWeight.w600,
-          color: CustomColors.onSurface,
+          color: CustomColors.pureWhite,
         ),
         titleMedium: TextStyle(
           fontSize: FontSize.large,
           fontFamily: PaintroidThemeData.fontFamily,
           fontWeight: FontWeight.w800,
-          color: CustomColors.primary,
+          color: CustomColors.oceanBlue,
         ),
         titleSmall: TextStyle(
           fontSize: FontSize.smallMedium,
           fontFamily: PaintroidThemeData.fontFamily,
           fontWeight: FontWeight.w500,
-          color: CustomColors.onSurface,
+          color: CustomColors.pureWhite,
         ),
       );
 
@@ -129,15 +129,15 @@ abstract class PaintroidThemeData {
 
   NavigationBarThemeData bottomNavBarThemeData = NavigationBarThemeData(
     indicatorColor: Colors.transparent,
-    backgroundColor: CustomColors.primary,
+    backgroundColor: CustomColors.oceanBlue,
     labelTextStyle: MaterialStateProperty.all(
       const TextStyle(
-        color: CustomColors.onSurface,
+        color: CustomColors.pureWhite,
       ),
     ),
     iconTheme: MaterialStateProperty.all(
       const IconThemeData(
-        color: CustomColors.onSurface,
+        color: CustomColors.pureWhite,
       ),
     ),
   );
