2b57353e5ea01727204f0c9f476f7095f2223c98
Baki Celebi <97812509+bakicelebi@users.noreply.github.com>
2024-06-29 12:49:14 +0200

    PAINTROID-743: Refactor ChangeNotifier providers (#83)
    * PAINTROID-744: refactor BrushToolStateData to PaintProvider
    
    * PAINTROID-744: fix tool switch bug & remove paint member from tools & refactor tools
    
    * PAINTROID-744: sort
    
    * PAINTROID-744: fix main.dart formatting
    
    * PAINTROID-744: fix tests
    
    * PAINTROID-743: refactor changenotifiers to generated class riverpod providers
    
    * PAINTROID-743: add recommendations from riverpod discord channel
    
    * PAINTROID-743: remove empty line
    
    * PAINTROID-743: sort

---
+++ lib/core/providers/object/canvas_painter_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'canvas_painter_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$canvasPainterProviderHash() =>
+    r'dee0580b6e68f1babfa1853fff0ef6a2764aff8f';
+
+/// See also [CanvasPainterProvider].
+@ProviderFor(CanvasPainterProvider)
+final canvasPainterProvider = AutoDisposeNotifierProvider<CanvasPainterProvider,
+    CanvasPainterProvider>.internal(
+  CanvasPainterProvider.new,
+  name: r'canvasPainterProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$canvasPainterProviderHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CanvasPainterProvider = AutoDisposeNotifier<CanvasPainterProvider>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/core/providers/state/app_bar_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_bar_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appBarProviderHash() => r'951b700b58e840e7cee4e46d91b67dd4008b723a';
+
+/// See also [AppBarProvider].
+@ProviderFor(AppBarProvider)
+final appBarProvider =
+    AutoDisposeNotifierProvider<AppBarProvider, AppBarProvider>.internal(
+  AppBarProvider.new,
+  name: r'appBarProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$appBarProviderHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AppBarProvider = AutoDisposeNotifier<AppBarProvider>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
