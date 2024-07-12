36e4c4fec984311db045d9fb891479e939515c83
Tim Celec <timcelec1@gmail.com>
2024-07-12 15:57:23 +0200

    PAINTROID-761 - Command works

---
+++ android/app/src/main/kotlin/org/catrobat/paintroid/FileReader.kt
@@ -0,0 +1,85 @@
+package org.catrobat.paintroid
+
+import android.content.Context
+import android.net.Uri
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.io.Input
+import com.esotericsoftware.kryo.io.Output
+import java.io.InputStream
+import org.catrobat.paintroid.model.Layer
+
+import org.catrobat.paintroid.command.serialization.VersionSerializer
+import org.catrobat.paintroid.command.Command
+class FileReader(private val context : Context)
+{
+    private val kryo = Kryo()
+    private val registerMap = LinkedHashMap<Class<*>, VersionSerializer<*>?>()
+    companion object {
+        const val MAGIC_VALUE = "CatrobatImg"
+        const val CURRENT_IMAGE_VERSION = 2 // handle 1 look up how to do it in the native verson
+    }
+    init {
+        setRegisterMapVersion(CURRENT_IMAGE_VERSION)
+      //  registerClasses()
+    }
+  /*  fun readFromFile(uri: String): CatrobatFileContent{
+        var commandModel: CommandManagerModel
+        var colorHistory: ColorHistory? = null
+    }*/
+  private fun setRegisterMapVersion(version: Int) {
+      // Only add new classes at the end
+      // because Kryo assigns an ID to each class
+      with(registerMap) {
+          put(Command::class.java, null)
+          /*put(CompositeCommand::class.java, CompositeCommandSerializer(version))
+          put(FloatArray::class.java, DataStructuresSerializer.FloatArraySerializer(version))
+          put(PointF::class.java, DataStructuresSerializer.PointFSerializer(version))
+          put(Point::class.java, DataStructuresSerializer.PointSerializer(version))
+          put(CommandManagerModel::class.java, CommandManagerModelSerializer(version))
+          put(SetDimensionCommand::class.java, SetDimensionCommandSerializer(version))
+          put(SprayCommand::class.java, SprayCommandSerializer(version))
+          put(Paint::class.java, PaintSerializer(version, activityContext))
+          put(AddEmptyLayerCommand::class.java, AddLayerCommandSerializer(version))
+          put(SelectLayerCommand::class.java, SelectLayerCommandSerializer(version))
+          put(LoadCommand::class.java, LoadCommandSerializer(version))
+          put(TextToolCommand::class.java, TextToolCommandSerializer(version, activityContext))
+          put(Array<String>::class.java, DataStructuresSerializer.StringArraySerializer(version))
+          put(FillCommand::class.java, FillCommandSerializer(version))
+          put(FlipCommand::class.java, FlipCommandSerializer(version))
+          put(CropCommand::class.java, CropCommandSerializer(version))
+          put(CutCommand::class.java, CutCommandSerializer(version))
+          put(ResizeCommand::class.java, ResizeCommandSerializer(version))
+          put(RotateCommand::class.java, RotateCommandSerializer(version))
+          put(ResetCommand::class.java, ResetCommandSerializer(version))
+          put(ReorderLayersCommand::class.java, ReorderLayersCommandSerializer(version))
+          put(RemoveLayerCommand::class.java, RemoveLayerCommandSerializer(version))
+          put(MergeLayersCommand::class.java, MergeLayersCommandSerializer(version))
+          put(PathCommand::class.java, PathCommandSerializer(version))
+          put(SerializablePath::class.java, SerializablePath.PathSerializer(version))
+          put(SerializablePath.Move::class.java, SerializablePath.PathActionMoveSerializer(version))
+          put(SerializablePath.Line::class.java, SerializablePath.PathActionLineSerializer(version))
+          put(SerializablePath.Quad::class.java, SerializablePath.PathActionQuadSerializer(version))
+          put(SerializablePath.Rewind::class.java, SerializablePath.PathActionRewindSerializer(version))
+          put(LoadLayerListCommand::class.java, LoadLayerListCommandSerializer(version))
+          put(GeometricFillCommand::class.java, GeometricFillCommandSerializer(version))
+          put(HeartDrawable::class.java, GeometricFillCommandSerializer.HeartDrawableSerializer(version))
+          put(OvalDrawable::class.java, GeometricFillCommandSerializer.OvalDrawableSerializer(version))
+          put(RectangleDrawable::class.java, GeometricFillCommandSerializer.RectangleDrawableSerializer(version))
+          put(StarDrawable::class.java, GeometricFillCommandSerializer.StarDrawableSerializer(version))
+          put(ShapeDrawable::class.java, null)
+          put(RectF::class.java, DataStructuresSerializer.RectFSerializer(version))
+          put(ClipboardCommand::class.java, ClipboardCommandSerializer(version))
+          put(SerializableTypeface::class.java, SerializableTypeface.TypefaceSerializer(version))
+          put(PointCommand::class.java, PointCommandSerializer(version))
+          put(SerializablePath.Cube::class.java, SerializablePath.PathActionCubeSerializer(version))
+          put(Bitmap::class.java, BitmapSerializer(version))
+          put(SmudgePathCommand::class.java, SmudgePathCommandSerializer(version))
+          put(ColorHistory::class.java, ColorHistorySerializer(version))
+          put(ClippingCommand::class.java, ClippingCommandSerializer(version))
+          put(LayerOpacityCommand::class.java, LayerOpacityCommandSerializer(version))
+     */ }
+  }
+
+
+
+}
---
+++ android/app/src/main/kotlin/org/catrobat/paintroid/contract/LayerContracts.kt
@@ -0,0 +1,155 @@
+/*
+ * Paintroid: An image manipulation application for Android.
+ * Copyright (C) 2010-2022 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.catrobat.paintroid.contract
+
+import android.graphics.Bitmap
+import android.view.View
+import android.widget.LinearLayout
+import androidx.annotation.StringRes
+
+
+interface LayerContracts {
+ /*   interface Adapter {
+        fun notifyDataSetChanged()
+
+        fun getViewHolderAt(position: Int): LayerViewHolder?
+    }
+
+    interface Presenter {
+        val layerCount: Int
+        val presenter: Presenter
+
+        fun onSelectedLayerInvisible()
+
+        fun onSelectedLayerVisible()
+
+        fun getListItemDragHandler(): ListItemDragHandler
+
+        fun refreshLayerMenuViewHolder()
+
+        fun disableVisibilityAndOpacityButtons()
+
+        fun getLayerItem(position: Int): Layer?
+
+        fun getLayerItemId(position: Int): Long
+
+        fun addLayer()
+
+        fun removeLayer()
+
+        fun changeLayerOpacity(position: Int, opacityPercentage: Int)
+
+        fun setLayerVisibility(position: Int, isVisible: Boolean)
+
+        fun refreshDrawingSurface()
+
+        fun setAdapter(layerAdapter: Adapter)
+
+        fun setDrawingSurface(drawingSurface: DrawingSurface)
+
+        fun invalidate()
+
+        fun setDefaultToolController(defaultToolController: DefaultToolController)
+
+        fun setBottomNavigationViewHolder(bottomNavigationViewHolder: BottomNavigationViewHolder)
+
+        fun isShown(): Boolean
+
+        fun onStartDragging(position: Int, view: View)
+
+        fun onStopDragging()
+
+        fun setLayerSelected(position: Int)
+
+        fun getSelectedLayer(): Layer?
+    }
+
+    interface LayerViewHolder {
+        val bitmap: Bitmap?
+        val view: View
+
+        fun setSelected(isSelected: Boolean)
+
+        fun updateImageView(layer: Layer)
+
+        fun setMergable()
+
+        fun isSelected(): Boolean
+
+        fun getViewLayout(): LinearLayout
+
+        fun bindView()
+
+        fun setLayerVisibilityCheckbox(setTo: Boolean)
+    }
+
+    interface LayerMenuViewHolder {
+        fun disableAddLayerButton()
+
+        fun enableAddLayerButton()
+
+        fun disableRemoveLayerButton()
+
+        fun enableRemoveLayerButton()
+
+        fun disableLayerOpacityButton()
+
+        fun disableLayerVisibilityButton()
+
+        fun isShown(): Boolean
+    }
+*/
+    interface Layer {
+        var bitmap: Bitmap
+        var isVisible: Boolean
+        var opacityPercentage: Int
+
+        fun getValueForOpacityPercentage(): Int
+    }
+
+    interface Model {
+        val layers: List<Layer>
+        var currentLayer: Layer?
+        var width: Int
+        var height: Int
+        val layerCount: Int
+
+        fun reset()
+
+        fun getLayerAt(index: Int): Layer?
+
+        fun getLayerIndexOf(layer: Layer): Int
+
+        fun addLayerAt(index: Int, layer: Layer): Boolean
+
+        fun listIterator(index: Int): ListIterator<Layer>
+
+        fun setLayerAt(position: Int, layer: Layer)
+
+        fun removeLayerAt(position: Int): Boolean
+
+        fun getBitmapOfAllLayers(): Bitmap?
+
+        fun getBitmapListOfAllLayers(): List<Bitmap?>
+    }
+
+    interface Navigator {
+        fun showToast(@StringRes id: Int, length: Int)
+    }
+}
