4ae4517d8c247091d11a66253f79b453edbbba52
Baki Celebi <clozopin@gmail.com>
2024-07-28 16:19:14 +0200

    PAINTROID-736 add shape command unit tests

---
+++ test/unit/command/shape_command_test.dart
@@ -0,0 +1,41 @@
+import 'dart:ui';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:paintroid/core/commands/command_implementation/graphic/shape/circle_shape_command.dart';
+import 'package:paintroid/core/commands/command_implementation/graphic/shape/rectangle_shape_command.dart';
+import 'shape_command_test.mocks.dart';
+
+@GenerateMocks([Canvas])
+void main() {
+  Paint testPaint = Paint();
+  late MockCanvas mockCanvas;
+  
+  final rectangleShapeCommand = RectangleShapeCommand(
+    testPaint,
+    const Offset(0, 0),
+    const Offset(200, 0),
+    const Offset(0, 200),
+    const Offset(200, 200),
+  );
+
+  const radius = 5.0;
+  const center = Offset(200, 200);
+  final circleShapeCommand = CircleShapeCommand(testPaint, radius, center);
+
+  setUp(() => mockCanvas = MockCanvas());
+
+  test('RectangleShapeCommand: Should call drawPath path and paint', () {
+    when(mockCanvas.drawPath(any, testPaint)).thenReturn(null);
+    rectangleShapeCommand.call(mockCanvas);
+    verify(mockCanvas.drawPath(any, testPaint));
+    verifyNoMoreInteractions(mockCanvas);
+  });
+
+  test('CircleShapeCommand: Should call drawOval with rect and paint', () {
+    when(mockCanvas.drawCircle(any, any, testPaint)).thenReturn(null);
+    circleShapeCommand.call(mockCanvas);
+    verify(mockCanvas.drawCircle(any, any, testPaint));
+    verifyNoMoreInteractions(mockCanvas);
+  });
+}
