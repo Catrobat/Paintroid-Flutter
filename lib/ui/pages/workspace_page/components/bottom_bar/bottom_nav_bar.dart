6365520d1e00b6e8a621c36a82d0fd64196619ee
Bhav Khurana <bhav.khurana.cse21@itbhu.ac.in>
2024-08-08 10:11:30 +0530

    minor changes

--- lib/ui/pages/workspace_page/components/bottom_bar/bottom_nav_bar.dart
+++ lib/ui/pages/workspace_page/components/bottom_bar/bottom_nav_bar.dart
@@ -5,9 +5,9 @@ import 'package:colorpicker/colorpicker.dart';
 
 import 'package:paintroid/core/enums/tool_types.dart';
 import 'package:paintroid/core/localization/app_localizations.dart';
+import 'package:paintroid/core/providers/state/paint_provider.dart';
 import 'package:paintroid/core/providers/state/tool_options_visibility_state_provider.dart';
-import 'package:paintroid/core/providers/state/tools/brush/brush_tool_state_provider.dart';
-import 'package:paintroid/core/providers/state/tools/toolbox/toolbox_state_provider.dart';
+import 'package:paintroid/core/providers/state/toolbox_state_provider.dart';
 import 'package:paintroid/core/tools/tool_data.dart';
 import 'package:paintroid/ui/pages/workspace_page/components/bottom_bar/bottom_nav_bar_items.dart';
 import 'package:paintroid/ui/pages/workspace_page/components/bottom_bar/tools/tools_bottom_sheet.dart';
@@ -23,7 +23,7 @@ class BottomNavBar extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final localizations = AppLocalizations.of(context);
     final currentToolData = getCurrentToolData(ref);
-    final currentPaint = ref.watch(brushToolStateProvider).paint;
+    final currentPaint = ref.watch(paintProvider);
 
     return NavigationBarTheme(
       data: PaintroidTheme.of(context).bottomNavBarThemeData,
@@ -126,9 +126,9 @@ void _showColorPicker(BuildContext context, WidgetRef ref) {
             topRight: Radius.circular(16.0),
           )),
       child: ColorPicker(
-        currentColor: ref.watch(brushToolStateProvider).paint.color,
+        currentColor: ref.watch(paintProvider).color,
         onColorChanged: (newColor) {
-          ref.read(brushToolStateProvider.notifier).updateColor(newColor);
+          ref.watch(paintProvider.notifier).updateColor(newColor);
         },
       ),
     ),
