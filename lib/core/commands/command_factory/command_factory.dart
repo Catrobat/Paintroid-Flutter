cd0d002f687aef44f27362476e3e0a1acb57a1ad
Bhav Khurana <bhav.khurana.cse21@itbhu.ac.in>
2024-08-19 17:25:03 +0530

    fix textstyle lint error

--- lib/core/commands/command_factory/command_factory.dart
+++ lib/core/commands/command_factory/command_factory.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 import 'package:paintroid/core/commands/command_implementation/graphic/line_command.dart';
 import 'package:paintroid/core/commands/command_implementation/graphic/path_command.dart';
