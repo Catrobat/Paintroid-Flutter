70bf01a1d0443e4909398e8dc45d5b727a7f5325
Lenkomotive <mario-kaurin@outlook.com>
2024-10-05 18:07:33 +0200

    PAINTROID-763: RedoStack not cleared for new project - extract back logic to top app bar

--- lib/ui/pages/workspace_page/workspace_page.dart
+++ lib/ui/pages/workspace_page/workspace_page.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:toast/toast.dart';
-
-import 'package:paintroid/core/providers/object/io_handler.dart';
 import 'package:paintroid/core/providers/state/workspace_state_notifier.dart';
 import 'package:paintroid/ui/pages/workspace_page/components/bottom_bar/bottom_nav_bar.dart';
 import 'package:paintroid/ui/pages/workspace_page/components/bottom_bar/tool_options/tool_options.dart';
 import 'package:paintroid/ui/pages/workspace_page/components/drawing_surface/drawing_canvas.dart';
 import 'package:paintroid/ui/pages/workspace_page/components/drawing_surface/exit_fullscreen_button.dart';
 import 'package:paintroid/ui/pages/workspace_page/components/top_bar/top_app_bar.dart';
-import 'package:paintroid/ui/shared/dialogs/discard_changes_dialog.dart';
+import 'package:toast/toast.dart';
 
 class WorkspacePage extends ConsumerStatefulWidget {
   const WorkspacePage({super.key});
@@ -39,30 +35,7 @@ class _WorkspaceScreenState extends ConsumerState<WorkspacePage> {
       workspaceStateProvider.select((state) => state.isFullscreen),
       (_, isFullscreen) => _toggleStatusBar(isFullscreen),
     );
-    final ioHandler = ref.watch(IOHandler.provider);
-    final workspaceStateNotifier = ref.watch(workspaceStateProvider.notifier);
     return PopScope(
-      canPop: false,
-      onPopInvoked: (didPop) async {
-        if (didPop) {
-          return;
-        }
-        if (isFullscreen) {
-          workspaceStateNotifier.toggleFullscreen(false);
-          return;
-        }
-        if (!workspaceStateNotifier.hasSavedLastWork) {
-          final shouldDiscard = await showDiscardChangesDialog(context);
-          if (shouldDiscard != null && !shouldDiscard && context.mounted) {
-            bool savedImage = await ioHandler.saveImage(context);
-            if (!savedImage) {
-              return;
-            }
-          }
-        }
-        if (!context.mounted) return;
-        Navigator.pop(context);
-      },
       child: Scaffold(
         appBar: isFullscreen ? null : const TopAppBar(title: 'Pocket Paint'),
         backgroundColor: Colors.grey.shade400,
