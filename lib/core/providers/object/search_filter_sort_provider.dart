d8abc37ffb1de26487480f71641d54fa1bd4d809
Lenkomotive <90652966+Lenkomotive@users.noreply.github.com>
2025-10-30 15:56:09 +0100

    Delete lib/core/providers/object/search_filter_sort_provider.dart

--- lib/core/providers/object/search_filter_sort_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:paintroid/core/models/sort_option.dart';
-
-final searchActiveProvider = StateProvider<bool>((ref) => false);
-final searchQueryProvider = StateProvider<String>((ref) => '');
-final sortOptionProvider = StateProvider<SortOption>(
-  (ref) => SortOption.dateModifiedNewest,
-);
