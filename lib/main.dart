629f53185f079a26662fa3c7e44f04b5bd19be35
Varun Kushwah <varunkushwah500notes@gmail.com>
2026-04-30 20:36:44 +0530

    [PAINTROID - 776] Second instance of app is opened when catrobat image is opened from files (#146)
    * fix(android): Second instance of app is opened when catrobat image is opened from files [PAINTROID-776]
    
    * style: Remove redundant import and use single quotes to satisfy linter
    
    * fix: Checks  unsaved project changes before loading new image

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logging/logging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -25,8 +25,17 @@ void main() async {
   );
 
   WidgetsFlutterBinding.ensureInitialized();
+  const platform = MethodChannel('org.catrobat.paintroid/file_handler');
+  String? initialFileUri;
+
+  try {
+    initialFileUri = await platform.invokeMethod('getInitialFile');
+  } on PlatformException catch (e) {
+    log("Failed to get initial file: '${e.message}'.");
+  }
+
   final prefs = await SharedPreferences.getInstance();
   final showOnboarding = prefs.getBool('showOnboarding') ?? true;
 
-  runApp(ProviderScope(child: App(showOnboardingPage: showOnboarding)));
+  runApp(ProviderScope(child: App(showOnboardingPage: showOnboarding,initialFileUri: initialFileUri)));
 }
