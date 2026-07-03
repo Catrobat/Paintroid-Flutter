25f673b47ddce3be1772af6a93e1989df9fbd69e
Stefan Knipfer <knipfer@student.tugraz.at>
2026-07-03 11:02:21 +0200

    [PAINTROID-809] Fix hidden help menu buttons

--- lib/ui/pages/onboarding_page/onboarding_page.dart
+++ lib/ui/pages/onboarding_page/onboarding_page.dart
@@ -62,87 +62,87 @@ class _OnboardingPageState extends State<OnboardingPage> {
     ToastContext().init(context);
 
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(bottom: 40),
-        child: Column(
-          children: [
-            Expanded(
-              child: PageView(
-                controller: _controller,
-                onPageChanged: (index) {
-                  setState(() => _isLastPage = index == 4);
-                },
-                children: const [
-                  Screen1(),
-                  Screen2(),
-                  Screen3(),
-                  Screen4(),
-                  Screen5(),
-                ],
-              ),
-            ),
-            Container(
-              height: 1,
-              color: PaintroidTheme.of(context).onSurfaceColor,
-            ),
-          ],
-        ),
-      ),
-      bottomSheet: Container(
-        color: PaintroidTheme.of(context).surfaceColor,
-        height: 40,
-        width: MediaQuery.of(context).size.width,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 30),
-              child: TextButton(
-                onPressed: () => finish(),
-                child: Text(
-                  _isLastPage ? '' : 'SKIP',
-                  style: TextStyle(
-                    color: PaintroidTheme.of(context).onSurfaceColor,
-                    fontSize: 15,
-                  ),
-                ),
-              ),
-            ),
-            SmoothPageIndicator(
-              count: 5,
+      backgroundColor: PaintroidTheme.of(context).surfaceColor,
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
               controller: _controller,
-              effect: SlideEffect(
-                dotColor: PaintroidTheme.of(context)
-                    .onSurfaceColor
-                    .withValues(alpha: 0.2),
-                dotHeight: 8,
-                dotWidth: 8,
-                activeDotColor: PaintroidTheme.of(context).onSurfaceColor,
-              ),
+              onPageChanged: (index) {
+                setState(() => _isLastPage = index == 4);
+              },
+              children: const [
+                Screen1(),
+                Screen2(),
+                Screen3(),
+                Screen4(),
+                Screen5(),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 30),
-              child: TextButton(
-                onPressed: () async {
-                  if (_isLastPage) {
-                    finish();
-                  } else {
-                    _controller.nextPage(
-                      duration: const Duration(milliseconds: 200),
-                      curve: Curves.easeInOut,
-                    );
-                  }
-                },
-                child: Text(
-                  _isLastPage ? "LET'S GO" : 'NEXT',
-                  style: TextStyle(
-                    color: PaintroidTheme.of(context).onSurfaceColor,
-                    fontSize: 15,
+          ),
+          Container(
+            height: 1,
+            color: PaintroidTheme.of(context).onSurfaceColor,
+          ),
+        ],
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          color: PaintroidTheme.of(context).surfaceColor,
+          height: 40,
+          width: MediaQuery.of(context).size.width,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: TextButton(
+                  onPressed: () => finish(),
+                  child: Text(
+                    _isLastPage ? '' : 'SKIP',
+                    style: TextStyle(
+                      color: PaintroidTheme.of(context).onSurfaceColor,
+                      fontSize: 15,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+              SmoothPageIndicator(
+                count: 5,
+                controller: _controller,
+                effect: SlideEffect(
+                  dotColor: PaintroidTheme.of(context)
+                      .onSurfaceColor
+                      .withValues(alpha: 0.2),
+                  dotHeight: 8,
+                  dotWidth: 8,
+                  activeDotColor: PaintroidTheme.of(context).onSurfaceColor,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 30),
+                child: TextButton(
+                  onPressed: () async {
+                    if (_isLastPage) {
+                      finish();
+                    } else {
+                      _controller.nextPage(
+                        duration: const Duration(milliseconds: 200),
+                        curve: Curves.easeInOut,
+                      );
+                    }
+                  },
+                  child: Text(
+                    _isLastPage ? "LET'S GO" : 'NEXT',
+                    style: TextStyle(
+                      color: PaintroidTheme.of(context).onSurfaceColor,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
